cff8e3da9513afd519858021254665ea2a536199
Alexander Chan <alexanderwc.chan@mail.utoronto.ca>
2026-03-09 21:02:26 -0400

    added monitoring dashboard

---
+++ dashboard/script.js
@@ -0,0 +1,42 @@
+function loadStats()
+{
+	fetch("http://10.0.0.190:5001/stats")
+	.then(res => res.json())
+	.then (data => {
+		console.log('DATA RECEIVED: ', data)
+		
+		const container = document.getElementById("nodes");
+		container.innerHTML = "";
+		
+		const size = data.totals.length;
+		
+		for (let i = 0; i < size; i++) {
+			const box = document.createElement("div");
+			box.className = "stat-box";
+			
+			if (data.cpuUsage[i] === -1 && data.ramUsage[i] === -1 && data.lastSpeed[i] === -1) {
+				box.innerHTML = `
+					<div class = "box-title">Node ${i}</div>
+					<div>Node finished working!</div>
+					<div>Average Cubes Per Second: ${data.averageSpeed[i].toFixed(3)}</div>
+				`;
+			}
+			else {
+				box.innerHTML = `
+					<div class = "box-title">Node ${i}</div>
+					<div>Cubes Solved: ${data.totals[i]}</div>
+					<div>Total Cubes To Solve: ${data.nodeGoals[i]}</div>
+					<div>Average Cubes Per Second: ${data.averageSpeed[i].toFixed(3)}</div>
+					<div>Cubes Per Second Since Last Report: ${data.lastSpeed[i].toFixed(3)}</div>
+					<div>CPU Usage: ${data.cpuUsage[i].toFixed(3)}%</div>
+					<div>RAM Usage: ${data.ramUsage[i].toFixed(3)}%</div>
+				`;
+			}
+			
+			container.appendChild(box);
+		}
+	})
+	.catch (err => {
+		console.log('Error: ', err);
+	});
+}
